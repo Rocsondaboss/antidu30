a124b36f994a6fd49d89399dfd1f0439d96df955
Rocsondaboss <61299589+Rocsondaboss@users.noreply.github.com>
2021-11-08 12:44:44 +0800

    Add files via upload

---
+++ top-10-funny-news-compilation.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Top 10 Funny News Compilation</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+<?php include 'includes/header.php'?>
+
+
+  <main id="main" data-aos="fade-in">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs">
+      <div class="container">
+        <h2>Top 10 Funny News Compilation</h2>
+        <p></p>
+      </div>
+    </div><!-- End Breadcrumbs -->
+
+    <!-- ======= Courses Section ======= -->
+    <section id="courses" class="courses">
+      <div class="container" data-aos="fade-up">
+
+        <h2>1. "Paano mo nasabi?"</h2>
+        <center><iframe width="560" height="315" src="https://www.youtube.com/embed/2_Cu5y8xiP4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+        <p>"Paano mo nasabi?" is the phrase used by the man identified as Robert Nocos. The man was arrested for allegedly stabbing a cashier and killing a waitress after she refused to have an sexual intercourse with him. The suspect gained prominence after generating the phrase "Paano mo nasabi?", which the man uses it to deviate the questions from the media who asked him. Since then, the man become the target of the memes by the online community and the YouTubers. The man was reportedly released from prison.</p>
+
+        <h2>2. "Bakit ako matatakot"</h2>
+        <center><iframe width="560" height="315" src="https://www.youtube.com/embed/2zARqkkCSXc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+        <p>This drunken man named Jun Calumpiano was arrested on June 7, 2016 after he reportedly fired his gun at the neighborhood. At the detention, he challenged then-president-elect Rodrigo Duterte to have a "gunfight."</p>
+
+        <h2>3. Imported ang tabas ng dila</h2>
+        <center><iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fchrishalyn.telic.31%2Fposts%2F553037459104110&show_text=true&width=500" width="500" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe></center>
+        <p>This June 2003 GMA News's <i>Saksi</i> report said that the man named Romano Matiyo was arrested for allegedly stealing the tricycle. At the police station, Matiyo, speaking in fluent English, claims that he is not stealing the tricycle but then deviates the question by talking about mathematical problems. Because the man talking weirdly, the police suspected that he was under the influence of drugs and his thoughts may have been warped.</p>
+        <p>That archived news video was originally available on YouTube but it was nowhere to be found (possibly it was taken down due to copyright infrigement from GMA News). However, the video is also available on TikTok.
+        
+        <h2>4. Pinoy Battousai</h2>
+        <center><iframe width="560" height="315" src="https://www.youtube.com/embed/ppioEEvJj-w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+
+        <p>Came from April 28, 2016 GMA News report, a man was dubbed as "Pinoy Battousai" after he used bolo to attack his friend. According to the report, the incident was captured on CCTV, where it shows the man named Chris Bautista taking care of his grandson, unaware that the imminent danger is coming for him. Seconds later, another man named Valentino Abiertas approaches with a bolo and attacks him. The children around them witness the incident in shock.</p>
+
+        <p>The two are later brought to the Barangay Captain's office. Bautista luckily did not injured in the attack, thanks for the "guardian angel" at his back. Abiertas explains that he used the bolo in opposite way based on his inspiration from Battousai (another name for Himura Kenshin), a Japanese character from Rurouni Kenshin. The motive for the attack was money issues. The two eventually reconciled.</p>
+
+        <h2>5. "Kademonyohan ng aking kamay"</h2>
+        <center><iframe width="560" height="315" src="https://www.youtube.com/embed/iVc5VTqA5e4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+        <p>A man Edwardo Valencio was arrested by the authorities for touching the two girls inside the jeepney. While being interviewed on his cell, he blames his "demonic" hand for the act. The man also admitted that he possessed illegal drugs. He was charged for "acts of lasciviousness."</p>
+
+        
+        
+        </div>
+
+      </div>
+    </section><!-- End Courses Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+ <?php include 'includes/footer.php'?>
+
+</body>
+
+</html>
